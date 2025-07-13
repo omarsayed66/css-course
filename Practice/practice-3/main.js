93d13d11afa475906c1aa499c3cd691599c1d833
omarsayed66 <bebo_om201080@hotmail.com>
2025-07-13 18:08:06 +0300

    Practice-3 Added Section Eight , Instagram , Before Footer And Footer With Responsive With Commnts

--- Practice/practice-3/main.js
+++ Practice/practice-3/main.js
@@ -1,3 +1,4 @@
+// .....................................Menu Toggle......................................................//
 const menu = document.getElementById("menu");
 const action = document.getElementById("action");
 const head = document.getElementById("head");
@@ -25,7 +26,6 @@ function omar() {
 }
 
 // ..............................................................................For Section Five
-
 const bigBox = document.getElementsByClassName("big-box");
 const bigBox2 = document.getElementsByClassName("big-box-2");
 const bigBox3 = document.getElementsByClassName("big-box-3");
@@ -92,29 +92,6 @@ const button2 = document.getElementById("button-2");
 
 const slides = document.getElementById("slides");
 
-// button1.addEventListener("click", () => {
-//   zr1();
-// });
-
-// function zr1() {
-//   slides.classList.remove("left-570");
-//   slides.classList.add("left-20");
-// }
-
-// // ...
-
-// button2.addEventListener("click", () => {
-//   zr2();
-// });
-
-// function zr2() {
-//   slides.classList.remove("left-20");
-
-//   slides.classList.add("left-570");
-// }
-
-// ...........................................................
-
 button1.addEventListener("click", () => {
   zr1();
 });
@@ -141,20 +118,26 @@ function zr2() {
   slide1.classList.add("no-active");
 }
 
-// button1.addEventListener("click", () => {
-//   zr1();
-// });
+// .......................................For Scroll Button...............................................
 
-// function zr1() {
-//   slides.classList.add("trans-60");
-// }
+// Get the button:
+let button = document.getElementById("button");
 
-// // ...
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// button2.addEventListener("click", () => {
-//   zr2();
-// });
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
 
-// function zr2() {
-//   slides.classList.remove("trans-60");
-// }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
